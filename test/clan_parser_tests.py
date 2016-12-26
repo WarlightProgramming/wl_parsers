9a2abfee38f02c2e1da6dea635954605814177e4
knyte <galactaknyte@gmail.com>
2016-12-26 12:07:28 -0600

    temp fix to file read issue

--- test/clan_parser_tests.py
+++ test/clan_parser_tests.py
@@ -13,8 +13,10 @@ from clan_parser import *
 def test_clanParser():
     cp = ClanParser(129)
     assert_equals(cp.URL, "https://www.warlight.net/Clans/?ID=129")
-    if ("/test" not in os.getcwd()):
-        os.chdir("./test")
+
+## tests actual parser functions
+@nottest
+def test_parserTools():
     testLoc = "data/corpdata.html"
     with open(testLoc, "rt") as fin:
         testData = fin.read()
