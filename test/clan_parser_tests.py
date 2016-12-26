c0683017eba79519a8f6f6199130135bc9f89b9d
knyte <galactaknyte@gmail.com>
2016-12-26 12:00:22 -0600

    Attempted fix to directory reference

--- test/clan_parser_tests.py
+++ test/clan_parser_tests.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append("..")
+import os
 
 # automated tests for clan_parser.py
 
@@ -12,6 +13,8 @@ from clan_parser import *
 def test_clanParser():
     cp = ClanParser(129)
     assert_equals(cp.URL, "https://www.warlight.net/Clans/?ID=129")
-    testLoc = "test/data/corpdata.html"
+    if ("/test" not in os.getcwd()):
+        os.chdir("./test")
+    testLoc = "data/corpdata.html"
     with open(testLoc, "rt") as fin:
         testData = fin.read()
