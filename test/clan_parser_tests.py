a221d20b5854068fe0232456384b2a43466aa951
knyte <galactaknyte@gmail.com>
2016-12-26 12:23:56 -0600

    added clan parser tests

--- test/clan_parser_tests.py
+++ test/clan_parser_tests.py
@@ -6,6 +6,7 @@ import os
 
 from nose.tools import *
 from clan_parser import *
+import datetime
 
 # main class tests
 
@@ -20,3 +21,15 @@ def test_parserTools():
     testLoc = "data/corpdata.html"
     with open(testLoc, "rt") as fin:
         testData = fin.read()
+    cp = ClanParser(129)
+    cp.pageData = testData
+    assert_equals(cp.getClanName(), "CORP")
+    assert_equals(cp.getMemberCount(), 87)
+    assert_equals(cp.getLink(), "https://www.warlight.net/Map/23141-Empire-CORP")
+    assert("from our ashes" in cp.getTagline())
+    assert_equals(cp.getCreatedDate(), datetime.date(2016, 6, 30))
+    assert ("Roleplayers of all kinds." in cp.getBio())
+    assert ((8246332261, "Prabster Realm", "Account Guardian", False) in cp.getMembers())
+
+if __name__ == "__main__":
+    test_parserTools()
